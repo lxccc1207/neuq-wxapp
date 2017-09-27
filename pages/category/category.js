ce338b9da29cfba3bf41e1484366530bcffb48ce
董子鑫 <dongzixin@huashantech.cn>
2017-09-27 22:00:43 +0800

    完善购物车修改和删除逻辑，新增分类列表查看

--- pages/category/category.js
+++ pages/category/category.js
@@ -1,31 +1,35 @@
 const api = require('../../api/index.js')
 Page({
     data: {
-        category: [
-            {name:'咖啡',id:'cafe'},
-            {name:'奶茶',id:'naicha'},
-            {name:'酸奶',id:'suannai'},
-            {name:'气泡水',id:'qipaoshui'},
-            {name:'奶昔',id:'naixi'},
-            {name:'特别推荐',id:'tebietuijian'}
-        ],
-        detail:[],
+        category: [],
+        list:[],
         curIndex: 0,
         isScroll: false,
         toView: 'cafe'
     },
     onLoad: function () {
+        var category, list
         api.getTypeList().then(res => {
-            if (res.data.status === 0) {
+            console.log(res)
+            category = res.data
+            api.getListCommodityByType(res.data[0].id).then(res => {
                 console.log(res)
-            }
+                list = res.data.res
+                this.setData({
+                    category: category,
+                    list: list
+                })
+            })
         })
     },
     switchTab(e){
-        this.setData({
-            toView : e.target.dataset.id,
-            curIndex : e.target.dataset.index
+        var id = e.currentTarget.dataset.id
+        api.getListCommodityByType(id).then(res => {
+            this.setData({
+                toView : e.target.dataset.id,
+                curIndex : e.target.dataset.index,
+                list: res.data.res
+            })
         })
     }
-    
 })
